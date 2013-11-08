cf955d5c6af8db77cb48f25d3ac24f8631588b21
Jenja20 <Jessalyn@d-69-91-146-236.dhcp4.washington.edu>
2013-11-08 15:02:26 -0800

    Added JS functions

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -11,22 +11,72 @@
 //sources on the same page
 var gov = gov || {};
 gov.usgs = gov.usgs || {};
-
-//base data URL--additional filters may be appended (see optional steps)
-//the SODA api supports the cross-origin resource sharing HTTP header
-//so we should be able to request this URL from any domain via AJAX without
-//having to use the JSONP technique
 gov.usgs.quakesUrl = 'https://soda.demo.socrata.com/resource/earthquakes.json?$$app_token=Hwu90cjqyFghuAWQgannew7Oi';
-
-//current earthquake dataset (array of objects, each representing an earthquake)
-gov.usgs.quakes;
-
-//reference to our google map
-gov.usgs.quakesMap;
+gov.usgs.quakes
+gov.usgs.quakesMap
 
 //AJAX Error event handler
 //just alerts the user of the error
 $(document).ajaxError(function(event, jqXHR, err){
     alert('Problem obtaining data: ' + jqXHR.statusText);
 });
+
+$(function(){
+	getQuakes();
+}); //onReady
+
+//getQuakes()
+//queries the server for the list of recent quakes
+//and plots them on a Google map
+function getQuakes() {
+	$.getJSON(gov.usgs.quakesUrl, function(quakes){
+	    gov.usgs.quakes = quakes;
+	    $(".message").html("Displaying" + quakes.length + "earthquakes");
+
+	    gov.usgs.quakesMap = new google.maps.Map($('.map-container')[0], {
+		    center: new google.maps.LatLng(0,0),        //centered on 0/0
+		    zoom: 2,                                    //zoom level 2
+		    mapTypeId: google.maps.MapTypeId.TERRAIN,   //terrain map
+		    streetViewControl: false                    //no street view
+		});
+
+	    addQuakeMarkers(quakes, gov.usgs.quakesMap);
+	}); //handle returned data function
+
+	$(".message").html("Loading... <img src=\"img/loading.gif\">");
+
+} //getQuakes()
+
+function addQuakeMarkers(quakes, map) {
+    
+    //loop over the quakes array and add a marker for each quake
+    var quake;      //current quake data
+       
+    for (var idx = 0; idx < quakes.length; ++idx) {
+        quake = quakes[idx];
+
+        quake.mapMarker = new google.maps.Marker({
+		    map: map,
+		    position: new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
+		});
+
+		google.maps.event.addListener(quake.mapMarker, 'click', function(){
+		    if(gov.usgs.iw) {
+		    	gov.usgs.iw.close();
+		    }
+
+		    //create an info window with the quake info
+			gov.usgs.iw = new google.maps.InfoWindow({
+			    content: new Date(quake.datetime).toLocaleString() + 
+			        ': magnitude ' + quake.magnitude + ' at depth of ' + 
+			        quake.depth + ' meters'
+			});
+
+			//open the info window
+			gov.usgs.iw.open(map, this);
+	                    
+	    }); //click handler for marker
+    }
+} //addQuakeMarkers()
+
 
